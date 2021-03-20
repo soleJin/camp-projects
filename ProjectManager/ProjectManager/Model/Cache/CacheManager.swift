49bd7272440edd75543e4e57cc8c3f5fed51bd0a
sole <subuchappyv2@gmail.com>
2021-03-21 03:49:15 +0900

    Feat: CacheManager-directoryURL property 추가 (#31)

--- ProjectManager/ProjectManager/Model/Cache/CacheManager.swift
+++ ProjectManager/ProjectManager/Model/Cache/CacheManager.swift
@@ -7,3 +7,10 @@
 
 import Foundation
 
+struct CacheManager {
+    static var directoryURL: URL {
+        let path: FileManager.SearchPathDirectory
+        path = .cachesDirectory
+        return FileManager.default.urls(for: path, in: .userDomainMask).first!
+    }
+}
