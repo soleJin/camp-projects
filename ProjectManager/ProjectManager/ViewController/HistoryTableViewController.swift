3574766f6c67dcf8c1b320f97978ef278f50be17
sole <subuchappyv2@gmail.com>
2021-03-20 21:04:43 +0900

    Feat: HistoryTableVC-configureView & configureAutoLayout method 구현 (#28)

--- ProjectManager/ProjectManager/ViewController/HistoryTableViewController.swift
+++ ProjectManager/ProjectManager/ViewController/HistoryTableViewController.swift
@@ -8,8 +8,32 @@
 import UIKit
 
 class HistoryTableViewController: UITableViewController {
+    
+    private var historyTableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(HistoryTableViewCell.self, forCellReuseIdentifier: HistoryTableViewCell.identifier)
+        tableView.separatorStyle = .none
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configrureView()
+        configureAutoLayout()
+    }
+    
+    private func configrureView() {
+        view.addSubview(historyTableView)
+    }
+    
+    private func configureAutoLayout() {
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            historyTableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            historyTableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            historyTableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            historyTableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
     }
 }
