42f7a961b1e689883d0671e69139487046421c69
sole <subuchappyv2@gmail.com>
2021-03-21 03:52:46 +0900

    Feat: CacheManager-store method 구현 (#31)

--- ProjectManager/ProjectManager/Model/Cache/CacheManager.swift
+++ ProjectManager/ProjectManager/Model/Cache/CacheManager.swift
@@ -13,4 +13,17 @@ struct CacheManager {
         path = .cachesDirectory
         return FileManager.default.urls(for: path, in: .userDomainMask).first!
     }
+    
+    static func store(_ list: [Todo], as fileName: ItemStatus) {
+        let url = directoryURL.appendingPathComponent(fileName.fileName, isDirectory: false)
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        do {
+            let data = try encoder.encode(list)
+            FileManager.default.createFile(atPath: url.path, contents: data, attributes: nil)
+        }
+        catch let error {
+            print("Failed to store: \(error.localizedDescription)")
+        }
+    }
 }
